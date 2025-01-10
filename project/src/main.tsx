ce01ca16989b575b6865af85b83fff89f2dbcc2b
WennnO <sophiafw2017@gmail.com>
2025-01-10 17:12:14 -0500

    first try translation

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -1,5 +1,8 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './i18n'; // Import before App
 import App from './App.tsx';
 import './index.css';
 
@@ -8,3 +11,11 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
+
+/*
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+*/
