648be6107d4420532372b0e1674eb83e9f761e9d
WennnO <sophiafw2017@gmail.com>
2025-02-13 18:54:39 -0500

    1

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
       <nav className="container mx-auto px-8 py-8 md:px-8 md:py-8">
         <div className="relative flex justify-between items-center">
           {/* Logo */}
-          <Link to="/" className="absolute left-2 flex items-center pt-4">
+            <Link to="/" className="absolute left-2 md:left-4 lg:left-8 flex items-center pt-4">
             <img 
               src={logo} 
               alt="Apollo Logo" 
@@ -28,7 +28,7 @@ const Header = () => {
 
           {/* Mobile Menu Button */}
           <button 
-            className="md:hidden p-2 text-white absolute top-2 right-4"
+            className="md:hidden p-2 text-white absolute top-1 right-2"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
             <svg 
