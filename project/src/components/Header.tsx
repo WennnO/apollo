5326f976ee590ae117d873def392d998da5eaae5
WennnO <sophiafw2017@gmail.com>
2025-02-13 20:13:36 -0500

    1

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
             <img 
               src={logo} 
               alt="Apollo Logo" 
-              className="h-24 w-144 object-contain" 
+              className="h-10 w-60 object-contain" 
             />
           </Link>
 
